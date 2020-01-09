aaac6bade8f0854f8f6e360829930e62cc6c9968
raviddog <raviddog@outlook.com>
2020-01-09 20:31:21 +1000

    enabled compiler warnings for gcc
    added both debug and release configurations for gcc
    
    #29

--- cmake_configure.sh
+++ cmake_configure.sh
@@ -1,8 +1,11 @@
 rm -rf ./build/gcc
+rm -rf ./build/gcc-release
 rm -rf ./build/vs
 
 mkdir ./build/gcc
+mkdir ./build/gcc-release
 mkdir ./build/vs
 
+cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-Og -g -Wall -Wpedantic -Wextra -Wformat" -S . -B ./build/gcc
+cmake -G "MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND" -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS="-w -O3" -S . -B ./build/gcc-release
 cmake -G "Visual Studio 16 2019" -S . -B ./build/vs
-cmake -G "MinGW Makefiles" -S . -B ./build/gcc -DCMAKE_SH="CMAKE_SH-NOTFOUND"
